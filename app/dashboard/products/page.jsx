9fd808bb81020e0511273e74a0c54cd98a609bba
Prashant <prashantdubey0098@gmail.com>
2023-11-24 00:46:34 +0530

    updated products and user page design

--- app/dashboard/products/page.jsx
+++ app/dashboard/products/page.jsx
@@ -1,5 +1,63 @@
+import Image from "next/image";
+import Link from "next/link";
+import styles from "@/app/ui/dashboard/products/products.module.css";
+import Search from "@/app/ui/dashboard/search/Search";
+import Pagination from "@/app/ui/dashboard/pagination/Pagination";
+
 function page() {
-  return <div>Products Page</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.top}>
+        <Search placeholder="Search for a product..." />
+        <Link href="/dashboard/products/add">
+          <button className={styles.addButton}>Add new</button>
+        </Link>
+      </div>
+      <table className={styles.table}>
+        <thead>
+          <tr>
+            <td>Title</td>
+            <td>Description</td>
+            <td>Price</td>
+            <td>Created At</td>
+            <td>Stock</td>
+            <td>Action</td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <div className={styles.product}>
+                <Image
+                  src="/noproduct.jpg"
+                  alt=""
+                  width="40"
+                  height={40}
+                  className={styles.productImage}
+                />
+                Iphone
+              </div>
+            </td>
+            <td>Desc</td>
+            <td>$999</td>
+            <td>13.01.2023</td>
+            <td>72</td>
+            <td className={styles.buttons}>
+              <Link href="/">
+                <button className={`${styles.button} ${styles.view}`}>
+                  View
+                </button>
+              </Link>
+              <button className={`${styles.button} ${styles.delete}`}>
+                Delete
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <Pagination />
+    </div>
+  );
 }
 
 export default page;
